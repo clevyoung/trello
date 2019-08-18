87781cc1f5164effd8f0835502680ebb9aec45ea
gldud <gl171111@gmail.com>
2019-08-18 12:13:13 +0900

    로그인 라우팅 처리

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import Login from "../components/Login.vue";
 
 Vue.use(VueRouter);
 
+//라우터 인스턴스 생성
 const router = new VueRouter({
-  routes: []
+  mode: "history",
+  routes: [{ path: "/login", component: Login }]
 });
 
 export default router;
