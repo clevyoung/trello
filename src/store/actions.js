234c8d091ca824368a287415ff97c943a6ee451f
HEE YOUNG <dud171111@gmail.com>
2019-08-19 23:44:17 +0900

    api호출하여 로그인 액션처리

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,7 +3,9 @@ import * as api from "../api";
 const actions = {
   LOGIN({ commit }, { email, password }) {
     //commit을 호출하여 mutations에 커밋
-    return api.login({ email, password }); //외부에서 참조할 수 있게 return
+    return api.login( email, password).then(({accessToken}) => {
+      commit('LOGIN', { accessToken })
+    }) //외부에서 참조할 수 있게 return
   }
 };
 
