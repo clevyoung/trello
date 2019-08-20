65707d4c80e8e4f6e6ea30567ceb4eb697bec576
gldud <gl171111@gmail.com>
2019-08-20 10:59:23 +0900

    fetch boards 액션 처리

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,9 +3,14 @@ import * as api from "../api";
 const actions = {
   LOGIN({ commit }, { email, password }) {
     //commit을 호출하여 mutations에 커밋
-    return api.login( email, password).then(({accessToken}) => {
-      commit('LOGIN', { accessToken })
-    }) //외부에서 참조할 수 있게 return
+    return api.login(email, password).then(({ accessToken }) => {
+      commit("LOGIN", { accessToken });
+    }); //외부에서 참조할 수 있게 return
+  },
+  FETCH_BOARDS({ commit }) {
+    return api.fetchBoard().then(data => {
+      commit("SET_BOARDS", data);
+    });
   }
 };
 
