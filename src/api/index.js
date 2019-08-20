65d394ec28faf5b6fd9bc245c4648b55f518d58b
gldud <gl171111@gmail.com>
2019-08-20 10:58:33 +0900

    보드 리스트 api연동

--- src/api/index.js
+++ src/api/index.js
@@ -1,18 +1,26 @@
 import axios from "axios";
-const domain = "http://localhost:3000"
+const domain = "http://localhost:3000";
 
 const request = (method, url, data) => {
   return axios({
     method,
     url,
     data
-  })
+  });
 };
 
-export const setAuthInHeader = (token) =>{
-  axios.defaults.headers.common['Authorization'] = token;
-}
+export const setAuthInHeader = token => {
+  axios.defaults.headers.common["Authorization"] = token;
+};
 
-export const login = (email, password ) => {
-  return request("post", `${domain}/login`, { email, password }).then(({data}) => data)
+export const login = (email, password) => {
+  return request("post", `${domain}/login`, { email, password }).then(
+    ({ data }) => data
+  );
+};
+
+export const fetchBoard = () => {
+  return request("get", `${domain}/boards`).then(res => {
+    return res.data;
+  });
 };
