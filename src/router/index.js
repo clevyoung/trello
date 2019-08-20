fdb7943837175a627d6c315701e9e5a5e762553a
gldud <gl171111@gmail.com>
2019-08-20 09:59:14 +0900

    Home 컴포넌트 라우팅 처리, 내비게이션 가드 설정

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Login from "../components/Login.vue";
+import Home from "../components/Home.vue";
 import store from "../store";
 
 Vue.use(VueRouter);
@@ -14,7 +15,10 @@ const requireAuth = (from, to, next) => {
 //라우터 인스턴스 생성
 const router = new VueRouter({
   mode: "history",
-  routes: [{ path: "/login", component: Login }]
+  routes: [
+    { path: "/login", component: Login },
+    { path: "/", component: Home, beforeEnter: requireAuth }
+  ]
 });
 
 export default router;
