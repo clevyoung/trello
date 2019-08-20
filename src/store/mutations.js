4792e9b0198ea1a874b77650374ec77dab0b46aa
gldud <gl171111@gmail.com>
2019-08-20 09:04:55 +0900

    로그아웃 상태처리

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,15 +1,18 @@
-import {setAuthInHeader} from "../api";
+import { setAuthInHeader } from "../api";
 
 const mutations = {
-    LOGIN(state, {accessToken}){
-        if(!accessToken) return; 
-        state.accessToken = accessToken;
-        //만약 token이 없을 시 return시킴
-        localStorage.accessToken = accessToken;
-        setAuthInHeader(accessToken)
-
-
-    }
-}
+  LOGIN(state, { accessToken }) {
+    if (!accessToken) return;
+    state.accessToken = accessToken;
+    //만약 token이 없을 시 return시킴
+    localStorage.accessToken = accessToken;
+    setAuthInHeader(accessToken);
+  },
+  LOGOUT(state) {
+    state.accessToken = null;
+    delete localStorage.accessToken;
+    setAuthInHeader();
+  }
+};
 
 export default mutations;
